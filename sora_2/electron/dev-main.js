4b91dd83715aa8a95e7523b9e5f21afee0cf8397
lemaster <130857181+lemasterr@users.noreply.github.com>
2025-11-24 22:53:19 +0100

    Delete sora_2 directory

--- sora_2/electron/dev-main.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-process.env.TS_NODE_PROJECT =
-  process.env.TS_NODE_PROJECT || path.join(__dirname, '..', 'tsconfig.electron.json');
-
-require('ts-node/register/transpile-only');
-require('./main');
